e8183cd8fce116ceed622b33dc702f82e5db069f
Matthew McGrath <rubywep@hotmail.com>
2021-08-29 20:14:05 -0700

    Fix animating title text to be responsive per-word rather than per-character

--- public/scripts/weather-form.js
+++ public/scripts/weather-form.js
@@ -12,21 +12,54 @@ cityInput.addEventListener('focusout', (event) => {
   cityInput.setAttribute('placeholder', cityInputPlaceholder);
 });
 
+// // Set the HTML of our Title to a series of spans, each with their own random animation properties
+// title.innerHTML = title.innerText
+//   .split('') // Split into array of letters
+//   .map((letter, index) => {
+//     let randomDelay = Math.floor(Math.random() * 1000);
+//     let randomDuration = Math.floor(Math.random() * 4000) + 2000;
+//     // let randomDuration = Math.floor(Math.random() * 1000).clamp(randomDelay, 500, 1000);
+//     // return `<span style="animation-delay:${index * 100}ms">${letter}</span>`;
+//     if (letter === ' ') {
+//       return `<span style="padding-left:25px; animation-delay:${0}ms; animation-duration:${randomDuration}ms">${letter}</span>`;
+//     }
+//     return `<span style="animation-delay:${0}ms; animation-duration:${randomDuration}ms">${letter}</span>`;
+//   })
+//   .join(''); // Turn array back into a string
+
+// const clamp = (num, min, max) => {
+//   return Math.min(Math.max(num, min), max);
+// };
+
 // Set the HTML of our Title to a series of spans, each with their own random animation properties
-title.innerHTML = title.innerText
-  .split('') // Split into array of letters
-  .map((letter, index) => {
-    let randomDelay = Math.floor(Math.random() * 1000);
-    let randomDuration = Math.floor(Math.random() * 4000) + 2000;
-    // let randomDuration = Math.floor(Math.random() * 1000).clamp(randomDelay, 500, 1000);
-    // return `<span style="animation-delay:${index * 100}ms">${letter}</span>`;
-    if (letter === ' ') {
-      return `<span style="padding-left:25px; animation-delay:${0}ms; animation-duration:${randomDuration}ms">${letter}</span>`;
-    }
-    return `<span style="animation-delay:${0}ms; animation-duration:${randomDuration}ms">${letter}</span>`;
-  })
-  .join(''); // Turn array back into a string
+
+const wordsInTitle = title.innerText.split(' ');
+console.log('Words: ', wordsInTitle);
+title.innerHTML = '';
+wordsInTitle.forEach((word) => {
+  console.log('WORD: ', word);
+  let wordHTML = `<div>`;
+  wordHTML += word
+    .split('')
+    .map((character, index) => {
+      console.log('CHAR: ', character);
+      return createSpan(character);
+    })
+    .join('');
+  wordHTML += `</div>`;
+
+  console.log('Final Word HTML: ', wordHTML);
+  title.innerHTML += wordHTML;
+});
 
 const clamp = (num, min, max) => {
   return Math.min(Math.max(num, min), max);
 };
+
+function createSpan(character) {
+  let randomDelay = Math.floor(Math.random() * 1000);
+  let randomDuration = Math.floor(Math.random() * 4000) + 2000;
+  let characterHTML = `<span style="animation-delay:${0}ms; animation-duration:${randomDuration}ms">${character}</span>`;
+  console.log('CHAR SPAN: ', characterHTML);
+  return `${characterHTML}`;
+}
